b2c24422a459d4fe38c7fd18b6f904fc53ca5594
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-06 17:25:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,19 @@ class Student
   def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
+    @id = id
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
