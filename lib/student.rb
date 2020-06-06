efc6ff659f10597efa82005f823ebb6b2a48eed0
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-06 17:52:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,9 +42,9 @@ class Student
   end
 
   def self.create(name:, grade:)
-    new_student = Student.new(name, grade)
-    new_student.save
-    new_student
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
   def self.new_from_db(row)
