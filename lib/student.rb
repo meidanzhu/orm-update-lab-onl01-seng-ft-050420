68770dc0c6c840ffd9bf33b09e1294665f25f8c5
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-06-06 17:46:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,9 @@ class Student
     Students.new(result[0], result[1], result[2])
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end
+
 end
